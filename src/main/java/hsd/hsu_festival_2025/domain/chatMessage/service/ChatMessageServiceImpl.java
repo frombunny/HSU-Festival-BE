994fa949f43f31f9259b9c5238905817970a7ea7
김혜진 <olivia019182@naver.com>
2025-05-06 01:56:31 +0900

    ✨ Feat: 챗봇 API에 비속어 필터링 기능 추가 - 정규 표현식 사용 (#13)

--- src/main/java/hsd/hsu_festival_2025/domain/chatMessage/service/ChatMessageServiceImpl.java
+++ src/main/java/hsd/hsu_festival_2025/domain/chatMessage/service/ChatMessageServiceImpl.java
@@ -5,7 +5,9 @@ import hsd.hsu_festival_2025.domain.chatMessage.entity.ChatOwner;
 import hsd.hsu_festival_2025.domain.chatMessage.repository.ChatMessageRepository;
 import hsd.hsu_festival_2025.domain.chatMessage.web.dto.SendMessageReq;
 import hsd.hsu_festival_2025.domain.chatMessage.web.dto.SendMessageRes;
+import hsd.hsu_festival_2025.domain.community.exception.BadWordException;
 import hsd.hsu_festival_2025.domain.openai.service.AnswerCreateService;
+import hsd.hsu_festival_2025.global.validator.BadWordValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,16 +20,19 @@ import java.time.LocalDateTime;
 public class ChatMessageServiceImpl implements ChatMessageService {
     private final ChatMessageRepository chatMessageRepository;
     private final AnswerCreateService answerCreateService;
+    private final BadWordValidator badWordValidator;
 
     // 새로운 메세지 전송
     @Override
     public SendMessageRes addNewChatMessage(SendMessageReq sendMessageReq) {
-        // TODO : 비속어 필터링
-        String question = sendMessageReq.getNewChat();
-
         ChatMessage questionMessage = ChatMessage.toEntity(sendMessageReq);
         // DB 저장 - 질문
         chatMessageRepository.save(questionMessage);
+
+        // 비속어 필터링
+        if(badWordValidator.containsBadWord(sendMessageReq.getNewChat())){
+            throw new BadWordException(); // 403 : 비속어 감지됨 - 요청 거부
+        }
 
         // AI 응답 생성
         String answer = answerCreateService.chat(sendMessageReq.getChatHistory(), sendMessageReq.getNewChat());
