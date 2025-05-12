e15bcf5cb721f8b487700b7fc4479c2e570cc934
frombunny <rucy011121@gmail.com>
2025-05-12 19:53:07 +0900

    fix: 잘못된 파일 삭제

--- src/main/java/hsd/hsu_festival_2025/domain/guestbook/web/dto/GuestBookMessageReq.java
+++
@@ -1,8 +0,0 @@
-package hsd.hsu_festival_2025.domain.guestbook.web.dto;
-
-public record GuestBookMessageReq(
-        String username,
-        String content
-){
-
-}
--- src/main/java/hsd/hsu_festival_2025/domain/guestbook/web/dto/GuestBookRes.java
+++
@@ -1,7 +0,0 @@
-package hsd.hsu_festival_2025.domain.guestbook.web.dto;
-
-public record GuestBookRes(
-        String username,
-        String content
-) {
-}
