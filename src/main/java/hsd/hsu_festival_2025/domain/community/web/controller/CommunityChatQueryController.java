4e49f215e9a5e77db0dcb109e46b300ca223b051
frombunny <rucy011121@gmail.com>
2025-05-12 21:22:59 +0900

    feat: 통신을 위한 requestParam 설정

--- src/main/java/hsd/hsu_festival_2025/domain/community/web/controller/CommunityChatQueryController.java
+++ src/main/java/hsd/hsu_festival_2025/domain/community/web/controller/CommunityChatQueryController.java
@@ -3,10 +3,7 @@ package hsd.hsu_festival_2025.domain.community.web.controller;
 import hsd.hsu_festival_2025.domain.community.service.CommunityMessageService;
 import hsd.hsu_festival_2025.domain.community.web.dto.CommunityMessageRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +14,14 @@ public class CommunityChatQueryController {
     private final CommunityMessageService communityMessageService;
 
     @GetMapping("/messages")
-    public List<CommunityMessageRes> getMessages(@CookieValue(value = "user_id", required = false) String userId) {
-        if (userId == null) {
-            return List.of();
-        }
+    public List<CommunityMessageRes> getMessages(
+            @CookieValue(value = "user_id", required = false) String userIdFromCookie,
+            @RequestParam(value = "user_id", required = false) String userIdFromParam
+    ) {
+        String userId = userIdFromCookie != null ? userIdFromCookie : userIdFromParam;
+        if (userId == null) return List.of();
+
         return communityMessageService.getRecentMessages(userId);
     }
+
 }
